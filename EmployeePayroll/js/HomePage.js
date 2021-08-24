53d7f87ed97635989e9f76174dbdc2a044096bdc
Diwakar <diwakar.n@tvsnext.io>
2021-08-24 17:04:05 +0530

    [Diwakar]Add:Render data using local json objects

--- EmployeePayroll/js/HomePage.js
+++ EmployeePayroll/js/HomePage.js
@@ -1,7 +1,18 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     createTableContents();
 });
-const createTableContents = () =>
+let employeePayrollList = [
+    {
+        _empName:'Diwakar',
+        _empGender:'Male',
+        _empDept:['Hr','Dev'],
+        _empSalary:'450000',
+        _startDate:'15 Dec 1999',
+        _notes:'',
+        _empProfilePic:'../assets/profile-images/Ellipse -5.png'
+    }
+]
+let createTableContents = () =>
 {
     const tableHeader = `<tr>
     <th>Image</th>
@@ -12,35 +23,33 @@ const createTableContents = () =>
     <th>Start date</th>
     <th>Actions</th>
 </tr>`;
-    const tableContents = `${tableHeader}<tr>
-    <td><img class="profile" src="../assets/profile-images/Ellipse -5.png" /></td>
-    <td>Diwakar N</td>
-    <td>Male</td>
-    <td>
-        <span class="dept_label">Dev</span>
-        <span class="dept_label">Finance</span>
-    </td>
-    <td>₹4500000</td>
-    <td>15 Dec 1999</td>
-    <td>
-        <img src="../assets/icons/delete-black-18dp.svg" alt="delete" />
-        <img src="../assets/icons/create-black-18dp.svg" alt="edit" />
-    </td>
-</tr>
-<tr>
-    <td><img class="profile" src="../assets/profile-images/Ellipse -5.png" /></td>
-    <td>Diwakar N</td>
-    <td>Male</td>
-    <td>
-        <span class="dept_label">Dev</span>
-        <span class="dept_label">Finance</span>
-    </td>
-    <td>₹4500000</td>
-    <td>15 Dec 1999</td>
-    <td>
-        <img src="../assets/icons/delete-black-18dp.svg" alt="delete" />
-        <img src="../assets/icons/create-black-18dp.svg" alt="edit" />
-    </td>
-</tr>`;
+let tableContents = `${tableHeader}`;
+    for(const emp of employeePayrollList)
+    {
+        tableContents = `${tableContents}<tr>
+            <td><img class="profile" src="${emp._empProfilePic}" /></td>
+            <td>${emp._empName}</td>
+            <td>${emp._empGender}</td>
+            <td>
+            ${getDept(emp._empDept)}
+            </td>
+            <td>${emp._empSalary}</td>
+            <td>${stringifyDate(emp._startDate)}</td>
+            <td>
+                <img src="../assets/icons/delete-black-18dp.svg" alt="delete" />
+                <img src="../assets/icons/create-black-18dp.svg" alt="edit" />
+            </td>
+        </tr>`;
+    }
+    
     document.getElementById('display_container').innerHTML = tableContents;
+}
+let getDept = (deptArr) =>
+{
+    let deptHtml = '';
+    for(const dept of deptArr)
+    {
+        deptHtml = `${deptHtml}<span class="dept_label">${dept}</span>`;
+    }
+    return deptHtml;
 }
